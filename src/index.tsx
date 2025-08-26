bfd2c5d4c54740a8c550b5ad5a325810d477e9ee
Rainer Simon <rainer@rainersimon.io>
2025-08-26 09:23:59 +0200

    Added example stylesheets

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,9 @@ import { App } from './app';
 
 import './styles/index.css';
 
-const URL = '/chapter_BCar_incl_sandhi.xml';
+// const URL = '/chapter_BCar_incl_sandhi.xml';
 // const URL = '/Rumi_Masnavi_1.xml';
-// const URL = '/True_udu82q.xml';
+const URL = '/True_udu82q.xml';
 // const URL = '/Videvdad.xml';
 
 ReactDOM.createRoot(document.getElementById('app')!).render(
