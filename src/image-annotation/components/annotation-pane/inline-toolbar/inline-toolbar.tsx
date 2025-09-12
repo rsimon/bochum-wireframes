b00c549ee0d282b841a7f21b077e6f73868992af
Rainer Simon <rainer@rainersimon.io>
2025-09-12 14:08:45 +0200

    Dummy concept tag

--- src/image-annotation/components/annotation-pane/inline-toolbar/inline-toolbar.tsx
+++ src/image-annotation/components/annotation-pane/inline-toolbar/inline-toolbar.tsx
@@ -1,8 +1,9 @@
-import { Ellipsis, MessagesSquare, Spline, Tag, Trash2 } from 'lucide-react';
+import { Ellipsis, MessagesSquare, Plus, Spline, Trash2, X } from 'lucide-react';
 import { ImageAnnotation, PopupProps, Store, useAnnotationStore } from '@annotorious/react';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
+import { Separator } from '@/components/ui/separator';
 
 interface InlineToolbarProps extends PopupProps {
 
@@ -18,23 +19,37 @@ export const InlineToolbar = (props: InlineToolbarProps) => {
     store.deleteAnnotation(props.annotation.id);
 
   return (
-    <div className="bg-white p-1.5 rounded-md 
+    <div className="bg-white p-1 rounded-lg flex gap-0.5
       shadow-[0_4px_12px_rgba(0,0,0,0.1),0_20px_40px_rgba(0,0,0,0.06)]">
       
+      <div className="text-xs tracking-wide font-semibold text-cyan-900 flex gap-1 px-1 items-center rounded-md bg-cyan-400">
+        <span className="pl-1.5">MyConcept</span>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="p-0 hover:bg-white/40 rounded-full size-7">
+          <X className="size-3" />
+        </Button>
+      </div>
+
       <Tooltip>
         <TooltipTrigger asChild>
           <Button
             variant="ghost"
             size="icon"
             className="relative">
-            <Tag className="size-3.5" />
+            <Plus className="size-3.5" />
           </Button>
         </TooltipTrigger>
 
         <TooltipContent>
-          <p>Add tag</p>
+          <p>Add concept tag</p>
         </TooltipContent>
       </Tooltip>
+
+      <div className="flex items-center mx-0.5"> 
+        <Separator orientation="vertical" />
+      </div>
 
       <Tooltip>
         <TooltipTrigger asChild>
@@ -60,7 +75,7 @@ export const InlineToolbar = (props: InlineToolbarProps) => {
             <MessagesSquare className="size-3.5" />
 
             <Badge 
-              className="absolute z-10 top-0 -right-[1px] text-[10px] font-semibold rounded-full aspect-square p-1 bg-blue-500 pointer-none:">3</Badge>
+              className="absolute z-10 top-0 -right-[1px] text-[10px] font-semibold rounded-full aspect-square p-1 bg-neutral-900 pointer-none:">3</Badge>
           </Button>
         </TooltipTrigger>
 
