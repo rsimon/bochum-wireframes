a5837ea6ce037d0167677833ff85827a4c139476
Rainer Simon <rainer@rainersimon.io>
2025-11-06 16:41:32 +0100

    Style tweaks

--- src/image-annotation/components/shared/relation-preview/relation-preview.tsx
+++ src/image-annotation/components/shared/relation-preview/relation-preview.tsx
@@ -44,12 +44,12 @@ export const RelationPreview = (props: RelationPreviewProps) => {
           openDelay={100}> 
           <HoverCardTrigger className="cursor-pointer">
             {category ? (
-              <div className="rounded-full text-xs font-medium py-0.5 px-1 flex items-center gap-1.5">
-                <div className={clsx('size-2 rounded-full', getCategoryColor(category))} />
+              <div className="rounded-full whitespace-nowrap text-xs font-medium py-0.5 px-1 flex items-center gap-1.5">
+                <div className={clsx('size-2 rounded-full shrink-0', getCategoryColor(category))} />
                 {category}
               </div>
             ) : (
-              <div className="size-2 mx-1 my-1.5 bg-gray-400 rounded-full " />
+              <div className="size-2 mx-1 my-1.5 bg-gray-400 rounded-full" />
             )}
           </HoverCardTrigger>
 
@@ -77,23 +77,25 @@ export const RelationPreview = (props: RelationPreviewProps) => {
       className={cn('flex justify-between bg-gray-50 items-center gap-1 border p-1 rounded-full shadow-xs', props.className)}>
       {isArrowLTR ? renderEdge(startAnnotation) : renderEdge(endAnnotation)}
 
-      <div className="grow relative self-stretch mb-0.5">
-        <div className="absolute w-full left-0 top-2.5 h-[1px] border-dashed border-t border-gray-400" />
+      <div className="grow relative self-stretch flex justify-center">
+        <div className="absolute w-full left-0 top-2.5 h-px border-dashed border-t border-gray-400" />
 
         {isArrowLTR ? (
           <div className="absolute -right-0.5 flex items-center h-full">
-            <div className="border-t-4 translate-y-[1px] border-t-transparent border-b-4 border-b-transparent border-l-8 border-l-gray-400" />
+            <div className="border-t-4 translate-y-px border-t-transparent border-b-4 border-b-transparent border-l-8 border-l-gray-400" />
           </div>
         ) : (
           <div className="absolute -left-0.5 flex items-center h-full">
-            <div className="border-t-4 translate-y-[1px] border-t-transparent border-b-4 border-b-transparent border-r-8 border-r-gray-400" />
+            <div className="border-t-4 translate-y-px border-t-transparent border-b-4 border-b-transparent border-r-8 border-r-gray-400" />
           </div>
         )}
 
         {category && (
-          <div className="text-xs h-full flex items-center justify-center relative">
-            <div className="bg-gray-50 font-light px-0.5">
-              {category}
+          <div className="text-xs h-full flex items-center justify-center relative max-w-[100px]">
+            <div className="flex items-centers min-w-0">
+              <div className="bg-gray-50 font-light px-0.5 whitespace-nowrap truncate">
+                {category}
+              </div>
             </div>
           </div>
         )}
